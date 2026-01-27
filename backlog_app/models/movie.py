8e920051c374c8c3f8fb9f5943230184e88d32b3
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-01-27 12:35:45 +0300

    add movie model

---
+++ backlog_app/models/movie.py
@@ -0,0 +1,57 @@
+# backlog_app/models/movie.py
+from datetime import datetime
+
+from sqlalchemy import String, Boolean, Integer, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from backlog_app.core.database import Base
+
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    title: Mapped[str] = mapped_column(
+        String(255),
+        nullable=False,
+        index=True,
+    )
+
+    description: Mapped[str | None] = mapped_column(
+        String(1000),
+        nullable=True,
+    )
+
+    year: Mapped[int | None] = mapped_column(
+        Integer,
+        nullable=True,
+    )
+
+    watched: Mapped[bool] = mapped_column(
+        Boolean,
+        default=False,
+        server_default="false",
+        nullable=False,
+    )
+
+    rating: Mapped[int | None] = mapped_column(
+        Integer,
+        nullable=True,
+    )
+
+    original_link: Mapped[str] = mapped_column(
+        String(255),
+        nullable=True,
+    )
+
+    kp_id: Mapped[int] = mapped_column(
+        Integer,
+        nullable=True,
+    )
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
