1dc9566dfff8777114de7370bf81ec05b7179041
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-01-27 13:37:21 +0300

    root endpoint

--- backlog_app/main.py
+++ backlog_app/main.py
@@ -1,8 +1,24 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from datetime import datetime
 from backlog_app.api import movie
 
 app = FastAPI(title="Backlog API")
+app_launch_time = datetime.now()
+
+@app.get("/", include_in_schema=False)
+async def root(request: Request):
+    uptime = (datetime.now() - app_launch_time).total_seconds()
+    docs_url = request.url.replace(
+        path="/docs",
+        query="",
+    )
+    return {
+        "status": "OK",
+        "current_time": datetime.now().isoformat(sep=" "),
+        "app_launch_time": int(uptime),
+        "docs_url": str(docs_url),
+    }
 
 app.include_router(movie.router)
 
