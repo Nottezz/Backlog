a16647de42d4340de18827898567b7b6675fb848
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-01-27 12:35:07 +0300

    add sqla engine

---
+++ backlog_app/core/database.py
@@ -0,0 +1,29 @@
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    AsyncSession,
+)
+from sqlalchemy.orm import DeclarativeBase
+
+from backlog_app.config import settings
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+engine = create_async_engine(
+    settings.db.connection.database_url_asyncpg,
+    echo=False,
+    pool_pre_ping=True,
+)
+
+AsyncSessionLocal = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+)
+
+
+async def get_session() -> AsyncSession:
+    async with AsyncSessionLocal() as session:
+        yield session
