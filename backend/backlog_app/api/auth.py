a3014191acb46e8acb7aad06965d88d089cea100
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-01-28 14:08:48 +0300

    add new routers: verify, reset password

--- backend/backlog_app/api/auth.py
+++ backend/backlog_app/api/auth.py
@@ -15,3 +15,11 @@ router.include_router(
 # /register
 router.include_router(
     fastapi_users.get_register_router(UserRead, UserCreate))
+
+# /request-verify-token /verify
+router.include_router(
+    fastapi_users.get_verify_router(UserRead))
+
+# /forgot-password # /reset-password
+router.include_router(
+    fastapi_users.get_reset_password_router())
