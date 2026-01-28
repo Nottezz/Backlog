63ed9db48241daf9ac44e34520805aa5b075d28a
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-01-28 13:05:52 +0300

    edit token url

--- backend/backlog_app/core/authentification/transport.py
+++ backend/backlog_app/core/authentification/transport.py
@@ -1,5 +1,4 @@
 from fastapi_users.authentication import BearerTransport
 
 bearer_transport = BearerTransport(
-    #todo: Update URL
-    tokenUrl="auth/jwt/login")
+    tokenUrl="/api/auth/login")
