cd0590e91e97e9f5ad690ab5290ce9d1a5a5bec1
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-01-28 16:29:14 +0300

    add the possibility of sending an email

--- backend/backlog_app/core/authentification/user_manager.py
+++ backend/backlog_app/core/authentification/user_manager.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING, Optional
 from config import settings
 from fastapi_users import BaseUserManager, UUIDIDMixin
 from models import User
+from mailing.tasks.email_task import send_verification_email, send_email_confirmed
 
 if TYPE_CHECKING:
     from fastapi import Request
@@ -31,4 +32,20 @@ class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
     ):
         logger.warning(
             "Verification requested for user %s. Verification token: %s", user.id, token
+        )
+
+        verification_link = "http://127.0.0.1:8000/docs#/Auth/verify_verify_api_auth_verify_post"
+        await send_verification_email.kiq(
+            user_id=str(user.id),
+            user_email=user.email,
+            verification_token=token,
+            verification_link=verification_link,
+        )
+
+    async def on_after_verify(self, user: User, request: Optional["Request"] = None):
+        logger.warning("User $s has been verified", user.id)
+
+        await send_email_confirmed.kiq(
+            user_id=str(user.id),
+            user_email=user.email,
         )
