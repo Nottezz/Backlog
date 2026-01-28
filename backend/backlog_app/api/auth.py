9b85355fb6c24cbbd9b37169aac8a2b507bbfad5
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-01-28 14:17:08 +0300

    add requires verification, but as commented out

--- backend/backlog_app/api/auth.py
+++ backend/backlog_app/api/auth.py
@@ -10,16 +10,17 @@ router = APIRouter(
 )
 # /login /logout
 router.include_router(
-    fastapi_users.get_auth_router(authentication_backend))
+    fastapi_users.get_auth_router(
+        authentication_backend,
+        # requires_verification=True,  # It is not necessary to include the parameter in this application.
+    )
+)
 
 # /register
-router.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate))
+router.include_router(fastapi_users.get_register_router(UserRead, UserCreate))
 
 # /request-verify-token /verify
-router.include_router(
-    fastapi_users.get_verify_router(UserRead))
+router.include_router(fastapi_users.get_verify_router(UserRead))
 
 # /forgot-password # /reset-password
-router.include_router(
-    fastapi_users.get_reset_password_router())
+router.include_router(fastapi_users.get_reset_password_router())
