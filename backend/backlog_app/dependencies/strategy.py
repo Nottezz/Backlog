41e5db3b87d091760c51415d6499ed6568986ed5
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-01-28 12:06:48 +0300

    create get database strategy dependency

--- backend/backlog_app/dependencies/strategy.py
+++ backend/backlog_app/dependencies/strategy.py
@@ -1,17 +1,18 @@
-import uuid
 from typing import TYPE_CHECKING, Annotated
 
 from fastapi import Depends
-from fastapi_users.authentication.strategy.db import AccessTokenDatabase, DatabaseStrategy
+from fastapi_users.authentication.strategy.db import DatabaseStrategy
 
 from config import settings
+from .access_tokens import get_access_token_db
 
 if TYPE_CHECKING:
     from models import AccessToken
+    from fastapi_users.authentication.strategy.db import AccessTokenDatabase
 
 
 def get_database_strategy(
-    access_token_db: Annotated[AccessTokenDatabase["AccessToken"], Depends(get_access_token_db)]
+    access_token_db: Annotated["AccessTokenDatabase[AccessToken]", Depends(get_access_token_db)]
 ) -> DatabaseStrategy:
     return DatabaseStrategy(access_token_db,
                             lifetime_seconds=settings.access_token_db.lifetime_seconds)
