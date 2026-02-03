0f4431e6eb0817d54dbe5b10773eed177f1028df
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-02-03 14:54:47 +0300

    add unit-test for root endpoint

---
+++ backend/tests/test_api/test_main_view.py
@@ -0,0 +1,7 @@
+from fastapi import status
+from fastapi.testclient import TestClient
+
+
+def test_root(client: TestClient) -> None:
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK, response.text
