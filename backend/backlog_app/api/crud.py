2f50941040493e6dc064e0131f7e4c99d95986b1
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-02-11 15:06:17 +0300

    refactor returning

--- backend/backlog_app/api/crud.py
+++ backend/backlog_app/api/crud.py
@@ -1,4 +1,5 @@
 import logging
+from uuid import UUID
 
 from fastapi import HTTPException
 from sqlalchemy import or_
@@ -8,7 +9,7 @@ from sqlalchemy.orm import selectinload
 
 from backlog_app.models import User
 from backlog_app.models.movie import Movie
-from backlog_app.schemas.movie import MovieCreate, MovieRead, MovieUpdate
+from backlog_app.schemas.movie import MovieCreate, MovieRead, MovieUpdate, MovieList
 
 logger = logging.getLogger(__name__)
 
@@ -16,29 +17,17 @@ logger = logging.getLogger(__name__)
 async def create_movie(
     db: AsyncSession, movie_in: MovieCreate, user: User
 ) -> MovieRead:
-    movie = Movie(**movie_in.model_dump(), user=user)
+    movie = Movie(**movie_in.model_dump(), user_id=user.id)
     db.add(movie)
     await db.commit()
     await db.refresh(movie)
 
     logger.info("Movie <%s> has been created.", movie.id)
 
-    return MovieRead(
-        id=movie.id,
-        title=movie.title,
-        description=movie.description,
-        year=movie.year,
-        rating=movie.rating,
-        kp_id=movie.kp_id,
-        imdb_id=movie.imdb_id,
-        watch_link=movie.watch_link,
-        watched=movie.watched,
-        created_at=movie.created_at,
-        user=user.email.split("@")[0],
-    )
+    return MovieRead.model_validate(movie)
 
 
-async def get_movies(db: AsyncSession, user_id: str | None = None):
+async def get_movies(db: AsyncSession, user_id: str | None = None) -> MovieList:
     query = select(Movie).options(selectinload(Movie.user))
 
     if user_id:
@@ -49,26 +38,13 @@ async def get_movies(db: AsyncSession, user_id: str | None = None):
     result = await db.execute(query)
     movies = result.scalars().all()
 
-    return [
-        MovieRead(
-            id=m.id,
-            title=m.title,
-            description=m.description,
-            year=m.year,
-            rating=m.rating,
-            imdb_id=m.imdb_id,
-            watch_link=m.watch_link,
-            watched=m.watched,
-            created_at=m.created_at,
-            kp_id=m.kp_id,
-            user=m.user.email.split("@")[0],
-        )
-        for m in movies
-    ]
+    logger.debug("Size of movies list: %s", len(movies))
+
+    return MovieList.model_validate({"movies": movies})
 
 
 async def get_movie_by_id(
-    db: AsyncSession, movie_id: int, user_id: int | None = None
+    db: AsyncSession, movie_id: int, user_id: UUID | None = None
 ) -> MovieRead | None:
     query = select(Movie).options(selectinload(Movie.user)).where(Movie.id == movie_id)
 
@@ -83,19 +59,7 @@ async def get_movie_by_id(
 
     logger.info("Movie has been found.")
 
-    return MovieRead(
-        id=movie.id,
-        title=movie.title,
-        description=movie.description,
-        year=movie.year,
-        rating=movie.rating,
-        imdb_id=movie.imdb_id,
-        watch_link=movie.watch_link,
-        watched=movie.watched,
-        created_at=movie.created_at,
-        kp_id=movie.kp_id,
-        user=movie.user.email.split("@")[0],
-    )
+    return MovieRead.model_validate(movie)
 
 
 async def update_movie(
@@ -122,19 +86,7 @@ async def update_movie(
 
     logger.info("Movie has been updated.")
 
-    return MovieRead(
-        id=movie.id,
-        title=movie.title,
-        description=movie.description,
-        year=movie.year,
-        rating=movie.rating,
-        kp_id=movie.kp_id,
-        imdb_id=movie.imdb_id,
-        watch_link=movie.watch_link,
-        watched=movie.watched,
-        user=movie.user.email.split("@")[0],
-        created_at=movie.created_at.isoformat(),
-    )
+    return MovieRead.model_validate(movie)
 
 
 async def partial_update_movie(
@@ -157,7 +109,7 @@ async def delete_movie(
     db: AsyncSession,
     movie_id: int,
     user: User,
-) -> None:
+) -> MovieRead:
     result = await db.execute(select(Movie).where(Movie.id == movie_id))
     movie = result.scalars().first()
     if not movie:
@@ -169,6 +121,8 @@ async def delete_movie(
     await db.commit()
 
     logger.info("Movie <%s> has been deleted.", movie_id)
+
+    return MovieRead.model_validate(movie)
 
 
 def check_movie_ownership(movie: Movie, user: User) -> None:
