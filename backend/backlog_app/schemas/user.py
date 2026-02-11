5311bd067b4d5583a22323285e1b7edbe7a975cf
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-02-11 15:05:16 +0300

    change UserRead schema

--- backend/backlog_app/schemas/user.py
+++ backend/backlog_app/schemas/user.py
@@ -1,12 +1,25 @@
-import uuid
 from typing import Annotated
+from uuid import UUID
 
 from annotated_types import Len
 from fastapi_users import schemas
+from pydantic import BaseModel, EmailStr, ConfigDict
+
+from .helper import to_camel_case
 
 
-class UserRead(schemas.BaseUser[uuid.UUID]):
-    pass
+class UserRead(BaseModel):
+    id: UUID
+    email: EmailStr
+
+    @property
+    def username(self):
+        return self.email.split('@')[0]
+
+    model_config = ConfigDict(
+        from_attributes=True,
+        alias_generator=to_camel_case
+    )
 
 
 class UserCreate(schemas.BaseUserCreate):
