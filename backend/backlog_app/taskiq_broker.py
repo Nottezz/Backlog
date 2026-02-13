d830225ae758a929c398f249ff30dfae4f6f340a
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-02-13 14:00:22 +0300

    add logging to taskiq

--- backend/backlog_app/taskiq_broker.py
+++ backend/backlog_app/taskiq_broker.py
@@ -1,4 +1,22 @@
+
+from taskiq import TaskiqEvents, TaskiqState
 from taskiq_aio_pika import AioPikaBroker
+import taskiq_fastapi
 from backlog_app.config import settings
+import logging
+
+logger = logging.getLogger(__name__)
 
 broker = AioPikaBroker(url=settings.taskiq.url)
+
+taskiq_fastapi.init(broker, "backlog_app.main:app")
+
+@broker.on_event(TaskiqEvents.WORKER_STARTUP)
+async def on_worker_startup(state: TaskiqState) -> None:
+    logging.basicConfig(
+        format=settings.logging.worker_log_format,
+        level=settings.logging.log_level,
+        datefmt=settings.logging.log_date_format,
+    )
+
+    logger.info("Worker startup complete, got state: %s", state)
