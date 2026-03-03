6600581e8fa79f5a0897299f71095b6dc5f86442
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-03-03 16:44:08 +0300

    add movie background task

---
+++ backend/backlog_app/tasks/movie_task.py
@@ -0,0 +1,32 @@
+import logging
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from backlog_app.api.crud import partial_update_movie
+from backlog_app.config import settings
+from backlog_app.schemas.movie import MovieRead, MovieUpdate
+from backlog_app.servicies.imdb_api.provider import IMDBProvider
+from backlog_app.storages.database import get_async_session
+
+logger = logging.getLogger(__name__)
+
+
+async def update_movie_rating(movie: MovieRead, db: AsyncSession):
+    provider = IMDBProvider(base_url=settings.imdb_url)
+
+    try:
+        imdb_rating, metacritic_score = await provider.get_title_rating(movie.title)
+    except Exception as e:
+        logger.error("Failed to fetch rating for movie <%s>: %s", movie.id, e)
+        return
+
+    await partial_update_movie(
+        db,
+        movie.id,
+        MovieUpdate(
+            imdb_rating=imdb_rating,
+            metacritic_score=metacritic_score,
+        ),
+    )
+
+    logger.info("Movie <%s> ratings updated in background", movie.id)
