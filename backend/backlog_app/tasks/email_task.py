91ef4b99f30a96ac98d8f7c40c73dcb02305fa6d
Nikita Yakovlev <driftworks2013@yandex.ru>
2026-03-04 16:17:01 +0300

    fix lint

--- backend/backlog_app/tasks/email_task.py
+++ backend/backlog_app/tasks/email_task.py
@@ -12,14 +12,12 @@ async def send_verification_email(
 ) -> None:
     subject = "Подтверждение регистрации на сайте backlog-movie.ru"
 
-    plain_content = dedent(
-        f"""\
+    plain_content = dedent(f"""\
             Здравствуйте!
             Пожалуйста, подтвердите ваш адрес электронной почты на сайте backlog-movie.ru, перейдя по ссылке: {verification_link}
 
             Администрация backlog-movie.ru
-        """
-    )
+        """)
     template = templates.get_template("email-verify/verification-request.html")
     context = {
         "verification_link": verification_link,
@@ -41,13 +39,11 @@ async def send_email_confirmed(
 ):
     subject = "Адрес электронной почты успешно подтверждён"
 
-    plain_content = dedent(
-        f"""\
+    plain_content = dedent(f"""\
         Здравствуйте!
         Ваш адрес электронной почты успешно подтверждён.
 
-        Администрация backlog-movie.ru"""
-    )
+        Администрация backlog-movie.ru""")
     template = templates.get_template("email-verify/email-verified.html")
     context = {
         "login_link": login_link,
@@ -67,13 +63,11 @@ async def send_email_forgot_password(
     user_email: str, reset_link: str, token_lifetime: str
 ):
     subject = "Запрос на сброс пароля на сайте backlog-movie.ru"
-    plain_content = dedent(
-        f"""\
+    plain_content = dedent(f"""\
             Здравствуйте!
             Мы получили запрос на сброс пароля. Перейдите по ссылке, чтобы задать новый пароль: {reset_link}
 
-            Администрация backlog-movie.ru"""
-    )
+            Администрация backlog-movie.ru""")
     template = templates.get_template("email-forgot/password-reset-request.html")
     context = {
         "reset_link": reset_link,
@@ -94,13 +88,11 @@ async def send_email_forgot_password_confirmed(
     user_email: str,
 ):
     subject = "Пароль был успешно изменён"
-    plain_content = dedent(
-        f"""\
+    plain_content = dedent(f"""\
             Здравствуйте!
             Ваш пароль был успешно изменён.
 
-            Администрация backlog-movie.ru"""
-    )
+            Администрация backlog-movie.ru""")
     template = templates.get_template("email-forgot/password-reset-confirmed.html")
     html_content = template.render()
 
